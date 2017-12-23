c316614571515f5b341c8d86a6fc9d8d82fe7d2d
drdlee <danielrudi.lee@gmail.com>
2017-12-24 04:17:03 +0800

    adding lodash

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
@@ -8,7 +9,6 @@ const API_KEY = 'AIzaSyD2eVWpbZ_vHlJNZ066OoumVA01fYnS0RU';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = { videos: [], selectedVideo: ''};
     this.onSearch('reactJS');
   }
@@ -20,9 +20,10 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.onSearch(term)}, 500);
     return (
       <div>
-        <SearchBar onSearch={(term) => this.onSearch(term)} />
+        <SearchBar onSearch={videoSearch} />
         <VideoDetail selVideo={this.state.selectedVideo} />
         <VideoList onVideoSelect={(selectVid) => this.setState({selectedVideo: selectVid})} videos={this.state.videos} />
       </div>
